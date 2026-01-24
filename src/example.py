fca5306c591049b35f13bb795ef2f79e84c39afc
Vitalii-Ohol <ohol.vitaliy@gmail.com>
2026-01-24 19:28:38 +0200

    testing device under vertain voltages

--- src/example.py
+++ src/example.py
@@ -13,20 +13,34 @@ ppk2_test.start_measuring()
 # the number of measurements in one sampling period depends on the wait between serial reads
 # it appears the maximum number of bytes received is 1024
 # the sampling rate of the PPK2 is 100 samples per millisecond
+idx = 2;
+voltages = [3000, 3200, 3300, 3600, 4000, 4200]
+ppk2_test.set_source_voltage(voltages[idx])
+
+total = 0
+count = 0
+
 while True:
     read_data = ppk2_test.get_data()
+
     if read_data != b'':
         samples, raw_digital = ppk2_test.get_samples(read_data)
-        total = sum(samples)/len(samples)
+        total += sum(samples)/len(samples)
+        count += 1
 
-        if total > 1_000_000:
-            print(f"{total/1_000_000:6.2f} A")
-        elif total > 1_000:
-            print(f"{total/1_000:6.2f} mA")
+    if count >= 1000:
+        final = total/count
+        count = 0
+        total = 0
+
+        if final > 1_000_000:
+            print(f"{voltages[idx]}: {final/1_000_000:6.2f} A")
+        elif final > 1_000:
+            print(f"{voltages[idx]}: {final/1_000:6.2f} mA")
         else:
-            print(f"{total:6.2f} uA")
+            print(f"{voltages[idx]}: {final:6.2f} uA")
 
-    time.sleep(.1)
+    time.sleep(.001)
 
 ppk2_test.toggle_DUT_power("OFF")
 ppk2_test.stop_measuring()
